e4598621b25dec2db907ead7f08f7e5a374157bd
duyguozpercin <duygucetind@gmail.com>
2025-05-09 14:03:24 +0300

    Add Tailwind CSS setup and refactor styles for QuoteCard and Button components

--- random-quote/src/components/Button/index.jsx
+++ random-quote/src/components/Button/index.jsx
@@ -1,5 +1,5 @@
 import './styles.css'
 
 export const Button = ({label, handleOnclick}) => {
-  return <button className="btn" onClick= {handleOnclick}>{label}</button>;
+  return <button className="bg-pink-400 text-white py-3 px-6 m-2.5 rounded-lg cursor-pointer text-base" onClick= {handleOnclick}>{label}</button>;
 }
--- random-quote/src/components/QuoteCard/index.js
+++ random-quote/src/components/QuoteCard/index.js
@@ -2,10 +2,10 @@ import './styles.css';
 
 export const QuoteCard = ({ quote, author, likeCount }) => {
   return (
-    <section className="QuoteCard">
-      <p className="quote-text">{quote}</p>
-      <p className="quote-author">— {author}</p>
-      <p className="like-count">💖 Likes: {likeCount}</p>
+    <section className="max-w-2xl bg-white p-10 rounded-xl text-sm my-5 mx-auto text-gray-600 ">
+      <p className="italic mb-5">{quote}</p>
+      <p className="text-slate-500">— {author}</p>
+      <p className="mt-4 text-fuchsia-400 text-xs">💖 Likes: {likeCount}</p>
     </section>
   );
 };
