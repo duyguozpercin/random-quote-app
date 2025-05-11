85988cd244e4265a82b581f4d3489146d324477e
duyguozpercin <duygucetind@gmail.com>
2025-05-11 11:38:34 +0300

    Enhance button styles in navigation for better visibility and update ProfilePage headings for improved emphasis

--- random-quote/src/pages/ProfilePage/index.jsx
+++ random-quote/src/pages/ProfilePage/index.jsx
@@ -8,12 +8,12 @@ export const ProfilePage = () => {
 
   return (
     <main>
-      <h1 className="text-lg my-5 mx-auto text-gray-600 text-center">Profile Page</h1>
+      <h1 className="text-lg font-bold my-5 mx-auto text-gray-600 text-center">Profile Page</h1>
       <div className="user-info">
         <p className="text-sm my-5 mx-auto text-gray-600 text-center">Welcome to your profile! Here's a list of the quotes you've liked so far 💖</p>
       </div>
 
-      <h2 className="text-sm my-5 mx-auto text-gray-600 text-center">Liked Quotes:</h2>
+      <h2 className="text-sm text-slate-700 my-5 mx-auto text-gray-600 text-center">Liked Quotes:</h2>
 
       {likedQuotes.length === 0 ? (
         <p>You haven't liked any quotes yet.</p>
