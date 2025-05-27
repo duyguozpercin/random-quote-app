18482c27da0fc4034f5aae44e286f3f47b9dec73
duyguozpercin <duygucetind@gmail.com>
2025-05-27 13:19:31 +0300

    refactor: define explicit types for QuoteCard component props

--- src/components/QuoteCard/index.tsx
+++ src/components/QuoteCard/index.tsx
@@ -1,6 +1,12 @@
 import { Quote } from '../../QuotesContextProvider';
 
-export const QuoteCard = ({ quote, author, likeCount }) => {
+interface QuoteCardProps {
+  quote: string;
+  author: string;
+  likeCount: number;
+}
+
+export const QuoteCard = ({ quote, author, likeCount }: QuoteCardProps) => {
   return (
     <section className="max-w-2xl bg-white p-10 rounded-xl text-sm my-5 mx-auto text-gray-600 text-center">
       <p className="italic mb-5">{quote}</p>
