58f101e879324f551efadd33a90199597b3452d0
duyguozpercin <duygucetind@gmail.com>
2025-06-05 00:01:30 +0300

    update Firebase configuration with hardcoded values for development

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,10 +3,13 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY || "YOUR_API_KEY",
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN || "YOUR_AUTH_DOMAIN",
-  projectId: process.env.REACT_APP_PROJECT_ID || "YOUR_PROJECT_ID",
-  appId: process.env.REACT_APP_APP_ID || "YOUR_APP_ID",
+  apiKey: "AIzaSyAtCufs_qX5am3aeR496dYWYw5pSv3LQ4Y",
+  authDomain: "random-quote-app-ace62.firebaseapp.com",
+  projectId: "random-quote-app-ace62",
+  storageBucket: "random-quote-app-ace62.firebasestorage.app",
+  messagingSenderId: "634003216396",
+  appId: "1:634003216396:web:1a30198aed352a835f7629",
+  measurementId: "G-9F69JH58HW"
 };
 
 const app = initializeApp(firebaseConfig);
